bc31aa775b8990f0ac82d291104f7137c45dd3e2
vhin0210 <vhin0210@gmail.com>
2013-09-20 13:19:06 +0800

    update photo content type display

--- sites/all/themes/mqcristobalawesome/templates/node--photo.tpl.php
+++ sites/all/themes/mqcristobalawesome/templates/node--photo.tpl.php
@@ -1,23 +1,23 @@
+<script>
+  (function($){
+    var collage = function(){
+      $('.field-name-field-photo-video .field-items').collagePlus();
+    }
+    $('.field-name-field-photo-video .field-items').ready(function(){
+      collage();
+    });
+    var resizeTimer = null;
+    $(window).bind('resize', function() {
+        // hide all the images until we resize them
+        // set the element you are scaling i.e. the first child nodes of ```.Collage``` to opacity 0
+        $('.field-name-field-photo-video .field-items .field-items').css("opacity", 0);
+        // set a timer to re-apply the plugin
+        if (resizeTimer) clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collage, 200);
+    });
+  })(jQuery);
+</script>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <script>
-    (function($){
-      var collage = function(){
-        $('.field-name-field-photo-video .field-items').collagePlus();
-      }
-      $('.field-name-field-photo-video .field-items').ready(function(){
-        collage();
-      });
-      var resizeTimer = null;
-      $(window).bind('resize', function() {
-          // hide all the images until we resize them
-          // set the element you are scaling i.e. the first child nodes of ```.Collage``` to opacity 0
-          $('.field-name-field-photo-video .field-items .field-items').css("opacity", 0);
-          // set a timer to re-apply the plugin
-          if (resizeTimer) clearTimeout(resizeTimer);
-          resizeTimer = setTimeout(collage, 200);
-      });
-    })(jQuery);
-  </script>
   <div class="social-buttons"><?php print _onix_share_get_social_links($nid); ?></div>
   <header>
     <?php print render($title_prefix); ?>
@@ -39,6 +39,7 @@
     hide($content['comments']);
     hide($content['links']);
     hide($content['field_tags']);
+    hide($content['field_photo_video']);
     print render($content);
   ?>
 
@@ -53,3 +54,4 @@
   
   <a href="#" class="node-next-button">&nbsp;</a>
 </article> <!-- /.node -->
+<?php print render($content['field_photo_video']); ?>
