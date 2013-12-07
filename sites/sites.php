cb948f1ce63e5105ccea0e7bc2842c608d8e6b24
vhin0210 <vhin0210@gmail.com>
2013-12-07 16:34:50 +0800

    v2 backend

--- sites/sites.php
+++ sites/sites.php
@@ -62,5 +62,9 @@
  $sites['vhinandrich.com.lottodemo'] = 'lottodemo.com';
  
  $sites['localhost.v2'] = 'v2.com';
+ $sites['localhost.v2.back'] = 'v2.com';
  $sites['127.0.0.1.v2'] = 'v2.com';
  $sites['vhinandrich.com.v2'] = 'v2.com';
+ $sites['localhost.v2.back'] = 'v2.com';
+ $sites['127.0.0.1.v2.back'] = 'v2.com';
+ $sites['vhinandrich.com.v2.back'] = 'v2.com';
