4fc7d6de91d09c0e206411870cbfc5f0f2ae0266
vhin0210 <vhin0210@gmail.com>
2014-01-13 16:17:11 +0800

    photos styles - parallax.js

--- sites/v2.com/themes/vhinandrich/template.php
+++ sites/v2.com/themes/vhinandrich/template.php
@@ -8,24 +8,29 @@ function vhinandrich_js_alter(&$javascript){
   global $user;
   
   $path = $_GET['q'];
-  if(strpos(strtolower($path),'admin')>-1 || $user->uid > 0){
-      //$jquery_module_path = drupal_get_path('module', 'jquery_update');
-      //$jquery_path_old = $jquery_module_path . '/replace/jquery/1.5/jquery.min.js'; //depends on the jquery version set
-      //$jquery_path_new = $jquery_module_path . '/replace/jquery/1.7/jquery.min.js'; //depends on the jquery version set
-      //$javascript[$jquery_path_old]['data'] = $jquery_path_new;
-  }else{
-    // Replace with current version.
-    if(module_exists('jquery_update')){
-      $jquery_module_path = drupal_get_path('module', 'jquery_update');
-      $jquery_path_old = $jquery_module_path . '/replace/jquery/1.5/jquery.min.js'; //depends on the jquery version set
-      $jquery_path_new = $jquery_module_path . '/replace/jquery/1.7/jquery.min.js'; //depends on the jquery version set
-      $javascript[$jquery_path_old]['data'] = $jquery_path_new;
-    }else{
-      $jQuery_version = '1.8.3'; //jquery 1.10 got some issues with the msie object
-      $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'bootstrap_subtheme').'/js/jquery-' . $jQuery_version . '.min.js';
-      $javascript['misc/jquery.js']['version'] = $jQuery_version;
+  foreach($javascript as $key => $script){
+    if(strstr($key, 'jquery.parallax.min.js') || strstr($key, 'waypoints.min.js')){
+        $javascript[$key]['scope'] = 'footer';
     }
   }
+  //if(strpos(strtolower($path),'admin')>-1 || $user->uid > 0){
+  //    //$jquery_module_path = drupal_get_path('module', 'jquery_update');
+  //    //$jquery_path_old = $jquery_module_path . '/replace/jquery/1.5/jquery.min.js'; //depends on the jquery version set
+  //    //$jquery_path_new = $jquery_module_path . '/replace/jquery/1.7/jquery.min.js'; //depends on the jquery version set
+  //    //$javascript[$jquery_path_old]['data'] = $jquery_path_new;
+  //}else{
+  //  // Replace with current version.
+  //  if(module_exists('jquery_update')){
+  //    $jquery_module_path = drupal_get_path('module', 'jquery_update');
+  //    $jquery_path_old = $jquery_module_path . '/replace/jquery/1.5/jquery.min.js'; //depends on the jquery version set
+  //    $jquery_path_new = $jquery_module_path . '/replace/jquery/1.7/jquery.min.js'; //depends on the jquery version set
+  //    $javascript[$jquery_path_old]['data'] = $jquery_path_new;
+  //  }else{
+  //    $jQuery_version = '1.8.3'; //jquery 1.10 got some issues with the msie object
+  //    $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'bootstrap_subtheme').'/js/jquery-' . $jQuery_version . '.min.js';
+  //    $javascript['misc/jquery.js']['version'] = $jQuery_version;
+  //  }
+  //}
 }
 
 function vhinandrich_preprocess_node(&$vars, $hook) {
@@ -91,6 +96,47 @@ function vhinandrich_preprocess_node_video__front_nodes(&$vars){
     $vars['id'] = end($urls);
 }
 
+function vhinandrich_preprocess_node_photo__front_nodes(&$vars){
+    $node = node_load($vars['nid']);
+    $vars['title'] = $node->title;
+    
+    $url = url('node/' . $vars['nid']);
+    $url = url('node/' . $vars['nid']);
+    $urls = explode('/', $url);
+    $vars['id'] = end($urls);
+    
+    $vars['photo_count'] = count($vars['field_photo']);
+    if($vars['photo_count'] > 1){
+        $vars['classes_array'][] = 'more-than-one';
+        
+        $newFieldPhoto = array();
+        $newFieldPhoto2 = array();
+        if($vars['photo_count'] < 12){
+            $multiplier = ($vars['photo_count'] % 4) * 4;
+            if($multiplier==0)
+                $multiplier = 4;
+        }else{
+            $multiplier = 1;
+        }
+        $fieldPhotoChildren = array_intersect_key($vars['content']['field_photo'], element_children($vars['content']['field_photo']));
+        for($i=0;$i<$multiplier;$i++){
+            $newFieldPhoto = array_merge($newFieldPhoto, $vars['field_photo']);
+            $newFieldPhoto2 = array_merge($newFieldPhoto2, $fieldPhotoChildren);
+        }
+        $vars['field_photo'] = $newFieldPhoto;
+        $vars['content']['field_photo']['#items'] = $newFieldPhoto;
+        array_walk($newFieldPhoto2, '_photoWeightUpdate');
+        $vars['content']['field_photo'] = array_merge($vars['content']['field_photo'], $newFieldPhoto2);
+    }
+    
+    
+    $vars['attributes_array']['data-photo-count'] = $vars['photo_count'];
+}
+function _photoWeightUpdate(&$value, $key){
+    $value['#weight'] = $key;
+}
+
+
 function vhinandrich_preprocess_field(&$vars){
     if($vars['element']['#view_mode'] == 'front_nodes' && $vars['element']['#bundle'] == 'video'){
         if($vars['element']['#field_name']=='field_video'){
@@ -106,6 +152,12 @@ function vhinandrich_preprocess_field(&$vars){
         }
         if($vars['element']['#field_name']=='body'){
             $vars['classes_array'][] = 'col-md-5';
+        }
+    }
+    if($vars['element']['#view_mode'] == 'front_nodes' && $vars['element']['#bundle'] == 'photo'){
+        if($vars['element']['#field_name'] == 'field_photo'){
+            $vars['attributes_array']['data-depth'] = '.8';
+            $vars['classes_array'][] = 'layer';
         }
     }
 }
