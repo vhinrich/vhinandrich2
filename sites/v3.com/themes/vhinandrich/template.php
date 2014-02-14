d5ef53454d827d2588c72b9f1cbd9cb7dc20a169
vhin0210 <vhin0210@gmail.com>
2014-02-14 23:38:07 +0800

    building version 3

--- sites/v3.com/themes/vhinandrich/template.php
+++ sites/v3.com/themes/vhinandrich/template.php
@@ -35,5 +35,21 @@ function vhinandrich_preprocess_node(&$vars, $hook) {
 
 
 function vhinandrich_preprocess_field(&$vars){
-  
+    $function = __FUNCTION__ . '_' . $vars['element']['#field_name'];
+    // for content type in general
+    if (function_exists($function))
+        $function($vars);
+    
+    // view_mode version
+    $teaserfunc = $function . '__' . $vars['element']['#view_mode'];
+    if (function_exists($teaserfunc))
+        $teaserfunc($vars);
+}
+
+function vhinandrich_preprocess_field_field_media__timeline(&$vars){
+    if(count($vars['items'])>1){
+        $vars['items'] = array(reset($vars['items']));
+        $vars['classes_array'][] = 'gallery';
+        $vars['theme_hook_suggestions'][] = 'field__field_media__gallery__article__timeline';
+    }
 }
