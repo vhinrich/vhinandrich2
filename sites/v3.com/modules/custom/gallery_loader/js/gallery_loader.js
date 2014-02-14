312113f46af523d4f3f4809c17442d8b6524f121
vhin0210 <vhin0210@gmail.com>
2014-02-15 02:24:19 +0800

    version 3 styles

--- sites/v3.com/modules/custom/gallery_loader/js/gallery_loader.js
+++ sites/v3.com/modules/custom/gallery_loader/js/gallery_loader.js
@@ -8,12 +8,17 @@
         e.preventDefault();
         var gallery = $(this).parents('.gallery');
         var nid = $(gallery).attr('data-node-id');
+        $('.gallery').removeClass('loading');
+        $(gallery).addClass('loading').delay(10000);
+        $('.field-item-overlay i', gallery).removeClass('fa-camera').addClass('fa-spinner').addClass('fa-spin');
         $.ajax(
           {
             url: Drupal.settings.basePath + 'node/7/gallery',
             success: function(data){
               $('.gallery-full').remove();
               $('.gallery').removeClass('active');
+              $('.gallery').removeClass('loading');
+              $('.field-item-overlay i', gallery).removeClass('fa-spin').removeClass('fa-spinner').addClass('fa-camera');
               $(gallery).addClass('active').after(data);
               $('.gallery-full').carousel();
             }
