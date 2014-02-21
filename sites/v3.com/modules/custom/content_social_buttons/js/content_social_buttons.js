be5ecf77bd71d6971dac488f4bfb2bcdb69ea312
vhin0210 <vhin0210@gmail.com>
2014-02-21 12:22:56 +0800

    social buttons

--- sites/v3.com/modules/custom/content_social_buttons/js/content_social_buttons.js
+++ sites/v3.com/modules/custom/content_social_buttons/js/content_social_buttons.js
@@ -3,22 +3,41 @@
     toggleShare: function(obj, state){
       if (state) {
         $('.content-social-buttons-wrapper').html('<h2>SHARE <span>' + $(obj).attr('data-title') + '</span></h2><div>' + $(obj).html() + '</div>');
-        //referesh like button
-        var socialElement = $('.content-social-buttons-wrapper');
-        FB.XFBML.parse(socialElement[0]);
-        
-        twttr.widgets.load();
-        
-        gapi.plusone.go();
       }else{
         $('.content-social-buttons-wrapper').html('');
       }
     },
+    refreshSocialButtons: function(){
+      if (typeof FB !== "undefined") {
+        var socialElement = $('.field-name-content-social-buttons .field-items .field-item');
+        $.each(socialElement, function(i, o){
+          if ($(o).hasClass('fb-rendered')) {
+            //code
+          }else{
+            $(o).addClass('fb-rendered');
+            FB.XFBML.parse(o);
+          }
+        });
+      }
+      
+      try{
+        if (typeof twttr !== "undefined")
+          twttr.widgets.load();
+      }catch(e){
+        console.log(e);
+      }
+      
+      if (typeof gapi !== "undefined")
+        gapi.plusone.go();
+    },
     attach: function(context, settings){
       
-      $('.field-name-content-social-buttons').click(function(e){
+      Drupal.behaviors.content_social_buttons.refreshSocialButtons();
+      
+      $('.field-name-content-social-buttons.btn').click(function(e){
         Drupal.behaviors.content_social_buttons.toggleShare($(this), !$('.row-offcanvas').hasClass('active'));
         $('.row-offcanvas').toggleClass('active');
+        
         //$('.row-offcanvas.active').click(function(e){
         //  Drupal.behaviors.social_buttons.toggleShare($(this), false);
         //  $('.row-offcanvas').removeClass('active');
--- sites/v3.com/themes/vhinandrich/template.php
+++ sites/v3.com/themes/vhinandrich/template.php
@@ -5,28 +5,32 @@
  */
 
 function vhinandrich_js_alter(&$scripts){
+  
+  //uglifyjs module
+  if(module_exists('uglifyjs')){
     if (variable_get('uglifyjs_skip_uglify', FALSE)) {
-    return;
-  }
-
-  $uglify_map = array();
-  if ($cache = cache_get('uglifyjs_map') && isset($cache) && count($cache->data)>0) {
-    $uglify_map = $cache->data;
-  }
-  else {
-    $uglify = vhinandrich_uglifyjs_info($scripts);
-
-    foreach ($uglify as $script) {
-      $uglify_map[$script] = array('data' => $script);
+      return;
     }
-    uglifyjs_uglify($uglify_map);
-    cache_set('uglifyjs_map', $uglify_map, 'cache', CACHE_TEMPORARY);
-  }
-
-  foreach ($uglify_map as $script) {
-    // If there was a problem minifying the script we won't make any changes.
-    if (isset($script['uglified_data']) && isset($scripts[$script['data']])) {
-      $scripts[$script['data']]['data'] = $script['uglified_data'];
+  
+    $uglify_map = array();
+    if ($cache = cache_get('uglifyjs_map') && isset($cache) && count($cache->data)>0) {
+      $uglify_map = $cache->data;
+    }
+    else {
+      $uglify = vhinandrich_uglifyjs_info($scripts);
+  
+      foreach ($uglify as $script) {
+        $uglify_map[$script] = array('data' => $script);
+      }
+      uglifyjs_uglify($uglify_map);
+      cache_set('uglifyjs_map', $uglify_map, 'cache', CACHE_TEMPORARY);
+    }
+  
+    foreach ($uglify_map as $script) {
+      // If there was a problem minifying the script we won't make any changes.
+      if (isset($script['uglified_data']) && isset($scripts[$script['data']])) {
+        $scripts[$script['data']]['data'] = $script['uglified_data'];
+      }
     }
   }
 }
@@ -36,30 +40,6 @@ function vhinandrich_preprocess_page(&$vars){
 }
 
 function vhinandrich_uglifyjs_info($scripts){
-    //$scripts = array(
-    //    // Core.
-    //    'misc/ajax.js',
-    //    'misc/authorize.js',
-    //    'misc/autocomplete.js',
-    //    'misc/batch.js',
-    //    'misc/collapse.js',
-    //    'misc/drupal.js',
-    //    'misc/form.js',
-    //    'misc/jquery.ba-bbq.js',
-    //    'misc/jquery.cookie.js',
-    //    'misc/jquery.form.js',
-    //    'misc/jquery.js',
-    //    'misc/jquery.once.js',
-    //    'misc/machine-name.js',
-    //    'misc/progress.js',
-    //    'misc/states.js',
-    //    'misc/tabledrag.js',
-    //    'misc/tableheader.js',
-    //    'misc/tableselect.js',
-    //    'misc/textarea.js',
-    //    'misc/timezone.js',
-    //    'misc/vertical-tabs.js'
-    //);
     $tmpScripts = array();
     foreach($scripts as $key => $script){
         if(strstr($key, '.js') && !strstr($key, '//')){
@@ -157,6 +137,16 @@ function vhinandrich_preprocess_field_field_media__default(&$vars){
 function vhinandrich_preprocess_field_content_social_buttons(&$vars){
   $vars['classes_array'][] = 'fa';
   $vars['classes_array'][] = 'fa-share';
+}
+
+function vhinandrich_preprocess_field_content_social_buttons__full(&$vars){
+  foreach($vars['classes_array'] as $key => $class){
+    if($class=='btn')
+      unset($vars['classes_array'][$key]);
+      
+    if($class=='fa-share')
+      unset($vars['classes_array'][$key]);
+  }
 }
 
 function vhinandrich_preprocess_field_content_social_button__timeline(&$vars){
