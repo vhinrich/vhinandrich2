6e92f72c6a03cb2879df817136f01a92bc1dc901
vhin0210 <vhin0210@gmail.com>
2014-02-24 22:33:34 +0800

    fit text

--- sites/v3.com/themes/vhinandrich/js/vhinandrich.js
+++ sites/v3.com/themes/vhinandrich/js/vhinandrich.js
@@ -2,7 +2,7 @@
     Drupal.behaviors.vhinandrich = {
         initFitText: function(){
             $(".page-header").fitText(1.1, { minFontSize: '40px', maxFontSize: '70px' });
-            $("#block-block-1 h1").fitText(1.1, { minFontSize: '40px', maxFontSize: '100px' });  
+            //$("#block-block-1 h1").fitText(1.1, { minFontSize: '40px', maxFontSize: '100px' });  
         },
         ajaxGAPush: function(){
             var d = document.location.pathname + document.location.search + document.location.hash;
