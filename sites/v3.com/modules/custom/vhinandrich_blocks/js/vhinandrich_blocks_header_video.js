f1453e92092f0d4e009797d102c21eeef731f34d
vhin0210 <vhin0210@gmail.com>
2014-09-24 18:02:25 +0800

    update video header block

--- sites/v3.com/modules/custom/vhinandrich_blocks/js/vhinandrich_blocks_header_video.js
+++ sites/v3.com/modules/custom/vhinandrich_blocks/js/vhinandrich_blocks_header_video.js
@@ -1,15 +1,22 @@
 (function($){
-  Drupal.behaviors.vhinandrich_blocks_video_header = {
-    initVideoHeader: function(context, settings){
+  Drupal.behaviors.vhinandrich_blocks_header_video = {
+    initHeaderVideo: function(context, settings){
       $(window).load(function(){
-        if($('#block-vhinandrich-blocks-vr-header-video .video-js > video > source').length===0){
+        console.log(settings);
+        if(
+          $('#block-vhinandrich-blocks-vr-header-video .video-js > video > source').length===0 ||
+          (
+            typeof settings.vhinandrich_blocks.header_video.poster_only !== 'undefined' &&
+            settings.vhinandrich_blocks.header_video.poster_only
+          )
+        ){
           $('#block-vhinandrich-blocks-vr-header-video .video-js > video').css('display', 'none');
           $('#block-vhinandrich-blocks-vr-header-video .video-js > .vjs-poster').css('display', 'block');
         }
       });
     },
     attach: function(context, settings){
-      this.initVideoHeader(context, settings);
+      this.initHeaderVideo(context, settings);
     }
   };
 })(jQuery);
--- sites/v3.com/modules/custom/vhinandrich_blocks/themes/vhinrich-videoheader.tpl.php
+++ sites/v3.com/modules/custom/vhinandrich_blocks/themes/vhinrich-videoheader.tpl.php
@@ -13,10 +13,8 @@ if (!empty($poster)) {
 }
 if (!empty($items)): ?>
 <video id="<?php print $player_id; ?>-video" data-setup="<?php print $data_setup; ?>" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" loop="loop" preload="auto"<?php echo $attrs; ?>>
-  <?php if(!isset($poster_only) || !$poster_only): ?>
-    <?php foreach ($items as $item): ?>
-      <source src="<?php print check_plain(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
-    <?php endforeach; ?>
-  <?php endif; ?>
+  <?php foreach ($items as $item): ?>
+    <source src="<?php print check_plain(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
+  <?php endforeach; ?>
 </video>
 <?php endif;
