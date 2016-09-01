4c3272cc7343bf0476fae6f40fe1754b94c28bee
Mark Cristobal <vhin0210@gmail.com>
2016-09-01 15:49:48 +0800

    - update contrib modules (mqcristobal & commerce)

--- sites/all/modules/contrib/media_browser_plus/js/media_browser_plus.js
+++ sites/all/modules/contrib/media_browser_plus/js/media_browser_plus.js
@@ -1,11 +1,11 @@
-/*!
+/**!
  * jQuery lightweight plugin boilerplate
  * Original author: @ajpiano
  * Further changes, comments: @addyosmani
  * Licensed under the MIT license
  * http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-patterns/
- */
-;(function ( $, window, document, undefined ) {
+ **/
+ ;(function ($, window, document, undefined) {
   // Create the defaults once
   var pluginName = 'mbp',
     defaults = {
@@ -185,7 +185,7 @@
         window.setTimeout(function(){
           $('.mbp-file-basket-list li', plugin.element).trigger('click');
         }, 1000);
-        window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/download-multiple/' + $('input[name=mbp_basket_files]', plugin.element).val();
+        window.location.href = this.getBasePath() + 'admin/content/file/download-multiple/' + $('input[name=mbp_basket_files]', plugin.element).val();
       });
 
       if (this.options.files_draggable) {
@@ -209,6 +209,9 @@
             .attr('checked', !$(this).find('input.vbo-select').attr('checked'))
             .trigger('change');
         }
+      })
+      .bind('dblclick.mbp', function(e) {
+        plugin['editFiles']($(this).find('input.vbo-select').val());
       });
     $('.mbp-file-list li input.vbo-select', this.element)
       .bind('change.mbp', function(e) {
@@ -266,6 +269,25 @@
     $('.mbp-action-', this.element).unbind('.mbp');
   };
 
+  /**
+   * Returns the proper Drupal path to use as destination parameter.
+   */
+  MBP.prototype.getDestinationPath = function () {
+    var path = window.location.pathname;
+    path = path.replace(new RegExp('^' + Drupal.settings.basePath.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')), '');
+    if (Drupal.settings.pathPrefix.length) {
+      path = path.replace(new RegExp('^' + Drupal.settings.pathPrefix.replace(/([.?*+^$[\]\\(){}|-])/g, '\\$1')), '');
+    }
+    return path;
+  };
+
+  /**
+   * Returns the proper Drupal path to navigate with.
+   */
+  MBP.prototype.getBasePath = function (path) {
+    return Drupal.settings.basePath + Drupal.settings.pathPrefix;
+  };
+
   MBP.prototype.folderOpen = function(folder) {
     $(folder)
       .addClass('open')
@@ -301,7 +323,7 @@
   MBP.prototype.deleteFiles = function () {
     var fids = this.getSelectedFiles();
     if (fids.length) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/delete-multiple/' + fids.join(' ');
+      window.location.href = this.getBasePath() + 'admin/content/file/delete-multiple/' + fids.join(' ') + '?destination=' + this.getDestinationPath();
     }
   }
 
@@ -315,17 +337,23 @@
     }
   }
 
-  MBP.prototype.editFiles = function () {
+  MBP.prototype.editFiles = function (fileId) {
     var fids = this.getSelectedFiles();
-    if (fids.length) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/edit-multiple/' + fids.join(' ');
+    if (typeof(fileId) != 'undefined') {
+      fids = [fileId];
+    }
+    if (fids.length > 1) {
+      window.location.href = this.getBasePath()+ 'admin/content/file/edit-multiple/' + fids.join(' ') + '?destination=' + this.getDestinationPath();
+    }
+    else if (fids.length == 1) {
+      window.location.href = this.getBasePath() + 'file/' + fids[0] + '/edit?destination=' + this.getDestinationPath();
     }
   }
 
   MBP.prototype.downloadFiles = function () {
     var fids = this.getSelectedFiles();
     if (fids.length) {
-      window.location.href = Drupal.settings.basePath + Drupal.settings.pathPrefix + 'admin/content/file/download-multiple/' + fids.join(' ');
+      window.location.href = this.getBasePath() + 'admin/content/file/download-multiple/' + fids.join(' ');
     }
   }
 
